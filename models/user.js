a274663e018b321d5caefaab560bb4fdff8a1fe7
jessesongok <jessesongok@gmail.com>
2020-05-26 14:21:30 +0300

    Added PG depecencies to package.json Added Validation and email Uniqueness to User Model

--- models/user.js
+++ models/user.js
@@ -3,7 +3,13 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      validate: {
+        notEmpty: true,
+      },
+    },
     password: DataTypes.STRING
   }, {});
   User.associate = function(models) {
