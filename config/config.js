082f42c784b4f462fd683619031971dcd6c3f1c2
jessesongok <jessesongok@gmail.com>
2020-05-26 14:38:32 +0300

    Changed DB config file to object

--- config/config.js
+++ config/config.js
@@ -15,7 +15,8 @@ module.exports = {
     "dialect": "mysql"
   },
   production: {
-    use_env_variable: process.env.DATABASE_URL,
+    "url" : process.env.DATABASE_URL,
     dialect: "postgres"
   }
+
 };
