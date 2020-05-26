9794c3e65fa3c5aa281c593cb759aa5d1c9df5e9
jessesongok <jessesongok@gmail.com>
2020-05-26 17:36:15 +0300

    Removed Auth0 authentication packages and configuration

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const { requiresAuth } = require('express-openid-connect');
 
 const router = express.Router();
 
-router.get('/profile', requiresAuth(), (req, res) => {
-  res.send(JSON.stringify(req.openid.user));
-});
+
 
 module.exports = router;
